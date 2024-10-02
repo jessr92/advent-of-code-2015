d6b5d0a46802fa77df33e65a4fa558949aae3f5e
Jessica Reid <jess.reid1992@outlook.com>
2024-10-02 20:50:47 +0100

    Formatting and additional type hints

--- src/day1.py
+++ src/day1.py
@@ -1,11 +1,12 @@
 def part_1(floors: str) -> int:
     return floors.count('(') - floors.count(')')
 
+
 def part_2(floors: str) -> int:
-    floor_number = 0
-    position = 1
+    floor_number: int = 0
+    position: int = 1
     for floor in floors:
-        floor_number = floor_number + 1  if floor == "("  else floor_number - 1
+        floor_number = floor_number + 1 if floor == "(" else floor_number - 1
         if floor_number < 0:
             break
         position += 1
