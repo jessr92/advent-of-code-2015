bb83c2a1763ef3eb6046e4e1416723b58b6fb2ec
Jessica Reid <jess.reid1992@outlook.com>
2024-10-02 21:05:50 +0100

    Formatting

--- tst/test_day_1.py
+++ tst/test_day_1.py
@@ -1,6 +1,7 @@
 import src.day1 as day1
 from utils import read_all_of_file
 
+
 def test_part_1_examples():
     assert day1.part_1("(())") == 0
     assert day1.part_1("()()") == 0
@@ -12,13 +13,16 @@ def test_part_1_examples():
     assert day1.part_1(")))") == -3
     assert day1.part_1(")())())") == -3
 
+
 def test_part_1():
     floors = read_all_of_file("day_1.txt")[0]
     assert day1.part_1(floors) == 138
 
+
 def test_part_2_examples():
     assert day1.part_2(")") == 1
     assert day1.part_2("()())") == 5
+
 
 def test_part_2():
     floors = read_all_of_file("day_1.txt")[0]
